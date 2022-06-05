40e5bed50b5368908d1e1fd9f8bde5bb3571de8e
Michele Onnis <michele.onnis@leonardo.com>
2022-06-05 18:26:45 +0200

    First, naive GET /books

--- src/main/java/onnis/samples/bookstore/controller/BookController.java
+++ src/main/java/onnis/samples/bookstore/controller/BookController.java
@@ -3,14 +3,16 @@ package onnis.samples.bookstore.controller;
 import onnis.samples.bookstore.model.Book;
 import onnis.samples.bookstore.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequestMapping(
         path = "/books",
-        produces = "application/json",
-        consumes = "application/json")
+        produces = "application/json")
 public class BookController {
     @Autowired
     private BookRepository bookRepo;
@@ -19,5 +21,11 @@ public class BookController {
     @ResponseStatus(HttpStatus.CREATED)
     Book createBook(@RequestBody Book book) {
         return bookRepo.save(book);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    Page<Book> books(Pageable pageable) {
+        return bookRepo.findAll(pageable);
     }
 }
