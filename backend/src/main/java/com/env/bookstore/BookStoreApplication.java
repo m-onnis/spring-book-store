947cd7197aca2ff0b0bd0d39c1790fb2a49591f7
Michele Onnis <michele.onnis@gmail.com>
2022-06-19 14:23:18 +0200

    nested JUnit tests for BookController

--- backend/src/main/java/com/env/bookstore/BookStoreApplication.java
+++ backend/src/main/java/com/env/bookstore/BookStoreApplication.java
@@ -1,15 +1,7 @@
 package com.env.bookstore;
 
-import com.env.bookstore.model.Author;
-import com.env.bookstore.model.Book;
-import com.env.bookstore.model.Publisher;
-import com.env.bookstore.repository.AuthorRepository;
-import com.env.bookstore.repository.BookRepository;
-import com.env.bookstore.repository.PublisherRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
--- backend/src/test/java/com/env/bookstore/controller/BookControllerTest.java
+++ backend/src/test/java/com/env/bookstore/controller/BookControllerTest.java
@@ -1,12 +1,16 @@
 package com.env.bookstore.controller;
 
+import com.env.bookstore.dto.BookCreationDto;
 import com.env.bookstore.model.Book;
 import com.env.bookstore.service.BookService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -15,8 +19,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = BookController.class)
@@ -30,31 +36,98 @@ class BookControllerTest {
     @MockBean
     private BookService bookService;
 
-    @Test
-    void books() throws Exception {
-        List<Book> bookList = Arrays.asList(
-                Book.builder().title("title1").isbn("isbn1").build(),
-                Book.builder().title("title2").isbn("isbn2").build());
+    @Nested
+    class GetBooks {
+        @Test
+        @DisplayName("should return book list with OK status")
+        void getBooksShouldReturnBookListAndOkStatus() throws Exception {
+            List<Book> bookList = Arrays.asList(
+                    Book.builder().title("title1").isbn("isbn1").build(),
+                    Book.builder().title("title2").isbn("isbn2").build());
 
-        // GIVEN
-        given(bookService.findAll()).willReturn(bookList);
+            given(bookService.findAll()).willReturn(bookList);
 
-        // WHEN
-        MvcResult result = mvc.perform(get("/books").contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
+            MvcResult result = mvc.perform(get("/books")
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(status().isOk())
+                    .andReturn();
 
-        // THEN
-        String responseBody = result.getResponse().getContentAsString();
-        String expectedBody = objectMapper.writeValueAsString(bookList);
-        assertThat(responseBody).isEqualToIgnoringWhitespace(expectedBody);
+            String actualBody = result.getResponse().getContentAsString();
+            String expectedBody = objectMapper.writeValueAsString(bookList);
+            assertThat(actualBody).isEqualToIgnoringWhitespace(expectedBody);
+        }
+
+        @Test
+        @DisplayName("should return book list (empty) with OK status")
+        void getEmptyBookLists() throws Exception {
+            MvcResult result = mvc.perform(get("/books")
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(status().isOk())
+                    .andReturn();
+
+            String actualBody = result.getResponse().getContentAsString();
+            String expectedBody = objectMapper.writeValueAsString(List.of());
+            assertThat(actualBody).isEqualToIgnoringWhitespace(expectedBody);
+        }
     }
 
-    @Test
-    void createBook() {
+    @Nested
+    class CreateBook {
+        @Test
+        @DisplayName("should succeed with valid parameter and return status CREATED")
+        void createBook() throws Exception {
+            BookCreationDto bookDto = BookCreationDto.builder()
+                    .title("title1")
+                    .isbn("isbn1")
+                    .authors(List.of(1L))
+                    .publisher(1L)
+                    .build();
+
+            MvcResult result = mvc.perform(post("/books")
+                            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .content(objectMapper.writeValueAsString(bookDto)))
+                    .andExpect(status().isCreated())
+                    .andReturn();
+        }
+
+        @Test
+        @DisplayName("should fail with invalid parameters and return status BAD REQUEST")
+        void createInvalidBook() throws Exception {
+            BookCreationDto bookDto = BookCreationDto.builder()
+                    // .title("title1")
+                    .isbn("isbn1")
+                    //.authors(List.of(1L))
+                    .publisher(1L)
+                    .build();
+
+            MvcResult result = mvc.perform(post("/books")
+                            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .content(objectMapper.writeValueAsString(bookDto)))
+                    .andExpect(status().isBadRequest())
+                    .andReturn();
+        }
     }
 
-    @Test
-    void deleteBook() {
+    @Nested
+    class DeleteBook {
+        @Test
+        @DisplayName("should delete an existing book and return OK")
+        void deleteBook() throws Exception {
+            MvcResult result = mvc.perform(delete("/books/{id}", "60")
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(status().isOk())
+                    .andReturn();
+        }
+
+        @Test
+        @DisplayName("should fail deleting an entity that doesn't exists and return NOT FOUND")
+        void deleteNotExistingBook() throws Exception {
+            doThrow(EmptyResultDataAccessException.class).when(bookService).delete(anyLong());
+
+            MvcResult result = mvc.perform(delete("/books/{id}", "60")
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(status().isNotFound())
+                    .andReturn();
+        }
     }
 }
