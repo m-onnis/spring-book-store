41783ca2bd6640647701a08ba498631e3be9178b
Michele Onnis <michele.onnis@gmail.com>
2022-06-19 10:34:48 +0200

    refine BookControllerTest.books

--- backend/src/test/java/com/env/bookstore/controller/BookControllerTest.java
+++ backend/src/test/java/com/env/bookstore/controller/BookControllerTest.java
@@ -2,36 +2,52 @@ package com.env.bookstore.controller;
 
 import com.env.bookstore.model.Book;
 import com.env.bookstore.service.BookService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Arrays;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(controllers = {BookController.class})
+@WebMvcTest(controllers = BookController.class)
 class BookControllerTest {
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private BookService bookService;
 
     @Test
     void books() throws Exception {
-        Book b1 = Book.builder().title("title1").isbn("isbn1").build();
-        Book b2 = Book.builder().title("title2").isbn("isbn2").build();
+        List<Book> bookList = Arrays.asList(
+                Book.builder().title("title1").isbn("isbn1").build(),
+                Book.builder().title("title2").isbn("isbn2").build());
 
-        given(bookService.findAll()).willReturn(Arrays.asList(b1, b2));
+        // GIVEN
+        given(bookService.findAll()).willReturn(bookList);
 
-        mvc.perform(get("/books").contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk());
+        // WHEN
+        MvcResult result = mvc.perform(get("/books").contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        // THEN
+        String responseBody = result.getResponse().getContentAsString();
+        String expectedBody = objectMapper.writeValueAsString(bookList);
+        assertThat(responseBody).isEqualToIgnoringWhitespace(expectedBody);
     }
 
     @Test
