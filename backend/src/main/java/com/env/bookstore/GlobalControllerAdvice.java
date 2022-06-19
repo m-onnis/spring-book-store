d20600821eef0ce2f739f21c73a7a02754ec3ac4
Michele Onnis <michele.onnis@gmail.com>
2022-06-19 14:22:56 +0200

    add general ControllerAdvice

---
+++ backend/src/main/java/com/env/bookstore/GlobalControllerAdvice.java
@@ -0,0 +1,16 @@
+package com.env.bookstore;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({ EmptyResultDataAccessException.class })
+    public void handleNotFound() {
+        // Nothing to do
+    }
+}
